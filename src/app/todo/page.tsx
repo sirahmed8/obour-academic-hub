4beeb3dac719dcd9972d3604289660c28576475d
sirahmed8 <a7medorabe7@gmail.com>
2026-01-07 08:48:42 +0200

    feat: Implement a new To-Do List page for task management.

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,4 +1,5 @@
 import { TodoList } from "@/components/features/todo/TodoList";
+import { AppShell } from "@/components/layout/AppShell";
 
 export const metadata = {
   title: "To-Do List | Obour Academic Hub",
@@ -6,5 +7,9 @@ export const metadata = {
 };
 
 export default function TodoPage() {
-  return <TodoList />;
+  return (
+    <AppShell>
+      <TodoList />
+    </AppShell>
+  );
 }
